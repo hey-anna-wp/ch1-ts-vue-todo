1497e3ccbf63b279db32c9cb64b1e2e52baaa94a
hey-anna-wp <217539366+hey-anna-wp@users.noreply.github.com>
2026-02-04 03:57:51 +0900

    feat: 히스토리 화면 Todo 리스트에 페이지네이션 적용 (#18)

--- src/components/todo/history/TodoListHistory.vue
+++ src/components/todo/history/TodoListHistory.vue
@@ -4,8 +4,9 @@ import type { Todo } from "@/types/todo"
 import TodoList from "@/components/todo/common/TodoList.vue"
 import HistoryToolbar from "./HistoryToolbar.vue"
 import TodoItemHistory from "./TodoItemHistory.vue"
-import { useSortOrder } from "@/composables/useSortOrder"
 import { sortTodos } from "@/utils/sortTodos"
+import { useSortOrder } from "@/composables/useSortOrder"
+import { usePagination } from "@/composables/usePagination"
 
 const props = defineProps<{ todos: Todo[] }>()
 const emit = defineEmits<{
@@ -19,6 +20,14 @@ const { sortOrder, toggleSortOrder } = useSortOrder("desc")
 const sortedTodos = computed(() => sortTodos(props.todos, sortOrder.value))
 
 const selectedIds = ref<Set<Todo["id"]>>(new Set())
+
+const { pagedItems, currentPage, totalPages, canPrev, canNext, rangeText, setPage, prev, next } =
+  usePagination<Todo>(sortedTodos, {
+    pageSize: 10,
+    resetPageOnItemsChange: true,
+  })
+
+watch(sortOrder, () => setPage(1))
 
 watch(
   () => props.todos.map((t) => t.id),
@@ -72,6 +81,14 @@ const purgeAll = () => {
     empty-text="보관된 할 일이 없습니다."
     :sort-order="sortOrder"
     @toggle-sort="toggleSortOrder"
+    :page="currentPage"
+    :total-pages="totalPages"
+    :can-prev="canPrev"
+    :can-next="canNext"
+    :range-text="rangeText"
+    @set-page="setPage"
+    @prev-page="prev"
+    @next-page="next"
   >
     <template #toolbar>
       <HistoryToolbar
@@ -85,7 +102,7 @@ const purgeAll = () => {
     </template>
 
     <TodoItemHistory
-      v-for="todo in sortedTodos"
+      v-for="todo in pagedItems"
       :key="todo.id"
       :todo="todo"
       :selected="selectedIds.has(todo.id)"
